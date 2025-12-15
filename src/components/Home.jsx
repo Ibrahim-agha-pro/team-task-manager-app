ed25d976131c5215f80bf03ee86182d098df54be
Ibrahim-agha-pro <ibrahim.agha.pro@gmail.com>
2025-12-15 14:34:50 +0300

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="p-8 max-w-3xl mx-auto">
-      <h2 className="text-3xl font-bold mb-4">Welcome to Team Manager</h2>
-      <p className="text-gray-600 mb-6">
-        Manage team members and their tasks quickly. Use the Team Members page
-        to add, edit, or remove members and tasks.
-      </p>
-      <Link
-        to="/teamlist"
-        className="inline-block px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700"
-      >
-        Go to Team Members
-      </Link>
+    <div className="min-h-screen flex justify-center items-center ">
+      <div className="p-8 max-w-3xl mx-auto">
+        <h2 className="text-3xl font-bold mb-4">Welcome to Team Manager</h2>
+        <p className="text-gray-600 mb-6">
+          Manage team members and their tasks quickly. Use the Team Members page
+          to add, edit, or remove members and tasks.
+        </p>
+        <Link
+          to="/teamlist"
+          className="inline-block px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700"
+        >
+          Go to Team Members
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="bg-gradient-to-r from-blue-600 to-indigo-600 text-white shadow-lg">
+    <nav className="bg-gradient-to-r fixed w-full from-blue-600 to-indigo-600 text-white shadow-lg">
       <div className="max-w-5xl mx-auto flex justify-between items-center px-6 py-4">
-        <h1 className="text-2xl font-extrabold tracking-wide">
+        <h1 className="text-md md:text-2xl font-extrabold tracking-wide">
           🚀 Team Manager
         </h1>
-        <div className="flex gap-6 text-lg items-center">
+        <div className="flex gap-2 md:gap-6 text-sm md:text-lg items-center">
           <Link
             to="/"
             className="hover:text-yellow-300 transition-colors font-medium"
